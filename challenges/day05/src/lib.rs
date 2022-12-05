379187d836bad24a1177ad41ff8313b6e7c5cef9
Conrad Ludgate <conrad.ludgate@truelayer.com>
2022-12-05 12:18:51 +0000

    optimise by walking backwards

--- challenges/day05/src/lib.rs
+++ challenges/day05/src/lib.rs
@@ -1,33 +1,11 @@
 #![feature(get_many_mut)]
 
-use std::mem::ManuallyDrop;
+use std::{array, mem::ManuallyDrop};
 
 use aoc::{Challenge, Parser as ChallengeParser};
 use arrayvec::{ArrayString, ArrayVec};
-use nom::{
-    branch::alt,
-    bytes::complete::tag,
-    character::{complete::anychar, streaming::line_ending},
-    sequence::tuple,
-    IResult, Parser,
-};
+use nom::{bytes::complete::tag, character::streaming::line_ending, sequence::tuple, IResult, Parser};
 use parsers::{number, ParserExt};
-
-#[derive(Debug, PartialEq, Clone, Copy)]
-struct Crate(u8);
-
-impl Crate {
-    fn parse(input: &'static str) -> IResult<&'static str, Self> {
-        alt((
-            tag("   ").map(|_| Crate(0)),
-            tuple((tag("["), anychar, tag("]"))).map(|(_, c, _)| Crate(c as u8)),
-        ))
-        .parse(input)
-    }
-}
-
-#[derive(Debug, PartialEq, Clone)]
-struct Stack(ArrayVec<Crate, 56>);
 
 #[derive(Debug, PartialEq, Clone, Copy)]
 struct Instruction {
@@ -38,75 +16,104 @@ struct Instruction {
 
 impl Instruction {
     fn parse(input: &'static str) -> IResult<&'static str, Self> {
-        tuple((tag("move "), number, tag(" from "), number, tag(" to "), number))
-            .map(|(_, count, _, from, _, to)| Instruction { count, from, to })
-            .parse(input)
+        tuple((
+            tag("move "),
+            number::<u8>,
+            tag(" from "),
+            number::<u8>,
+            tag(" to "),
+            number::<u8>,
+        ))
+        .map(|(_, count, _, from, _, to)| Instruction {
+            count,
+            from: from - 1,
+            to: to - 1,
+        })
+        .parse(input)
     }
 }
 
 #[derive(Debug, PartialEq, Clone)]
 pub struct Day05 {
-    stacks: ArrayVec<Stack, 9>,
+    data: &'static str,
+    stacks: usize,
     instructions: ArrayVec<Instruction, 512>,
 }
 
 impl ChallengeParser for Day05 {
-    fn parse(mut input: &'static str) -> IResult<&'static str, Self> {
-        let lines: ArrayVec<ArrayVec<Crate, 9>, 8>;
-        (input, lines) = Crate::parse
-            .separated_list1(tag(" "))
-            .terminate_list1(line_ending)
-            .parse(input)?;
+    fn parse(input: &'static str) -> IResult<&'static str, Self> {
+        let line_length = input.find('\n').unwrap() + 1;
+        let block_length = input.find("\n\n").unwrap() + 1;
+        let stacks = line_length / 4;
 
-        let mut stacks = ArrayVec::<Stack, 9>::new();
-        for (i, line) in lines.into_iter().enumerate() {
-            for (j, krate) in line.into_iter().enumerate() {
-                if i == 0 {
-                    let stack = ArrayVec::new();
-                    stacks.push(Stack(stack));
-                }
-                stacks[j].0.insert(0, krate);
-            }
-        }
-
-        for stack in &mut stacks {
-            while stack.0.last() == Some(&Crate(0)) {
-                stack.0.pop();
-            }
-        }
-
-        // skip empty lines
-        input = &input[4 * stacks.len() + 1..];
+        let (data, input) = input.split_at(block_length - line_length);
+        let (_, input) = input.split_at(line_length + 1);
 
         let (input, instructions) = Instruction::parse.separated_list1(line_ending).parse(input)?;
-        Ok((input, Self { stacks, instructions }))
+        Ok((
+            input,
+            Self {
+                data,
+                stacks,
+                instructions,
+            },
+        ))
     }
 }
 
 impl Day05 {
     #[inline(always)]
     fn solve(self, reverse: bool) -> ArrayString<9> {
-        let Self { stacks, instructions } = self;
-        let mut stacks = ManuallyDrop::new(stacks);
+        let Self {
+            data,
+            stacks,
+            instructions,
+        } = self;
+
+        // manually drop - ArrayVec is bad at eliminating dropbounds so I will enforce it here :)
         let instructions = ManuallyDrop::new(instructions);
 
-        for i in &*instructions {
-            // let (Stack(from), Stack(to)) = slice_dual_mut(&mut stacks, i.from as usize - 1, i.to as usize - 1);
-            let [Stack(from), Stack(to)] = stacks.get_many_mut([i.from as usize - 1, i.to as usize - 1]).unwrap();
+        // the value of state is (X, Y) where X is the stack this value is in
+        // and Y is the height (offset) in the stack
+        let mut states: [_; 9] = array::from_fn(|i| (i as u8, 128));
+        // current 'length' of the stack - not accurate but is relatively correct
+        let mut lengths = [129_u8; 9];
 
-            if reverse {
-                for _ in 0..i.count {
-                    to.push(from.pop().unwrap())
+        // walk backwards
+        for i in instructions.iter().rev().copied() {
+            // for all of our states
+            for state in &mut states {
+                let offset = lengths[i.to as usize].wrapping_sub(state.1);
+                // if the state is in the resulting stack and is count from the top
+                if state.0 == i.to && offset <= i.count {
+                    // calculate the new offset from the top of the new stack
+                    let offset = if reverse { offset - 1 } else { i.count - offset };
+                    let height = lengths[i.from as usize].wrapping_add(offset);
+                    *state = (i.from, height);
                 }
-            } else {
-                let partition = from.len() - i.count as usize;
-                to.extend(from.drain(partition..));
+            }
+            // adjust stack lengths
+            lengths[i.from as usize] = lengths[i.from as usize].wrapping_add(i.count);
+            lengths[i.to as usize] = lengths[i.to as usize].wrapping_sub(i.count);
+        }
+
+        // calculate lengths of each stack based on the data
+        let mut data_index_offsets = [0; 9];
+        for line in data.as_bytes().chunks(stacks * 4) {
+            for (stack, length) in data_index_offsets.iter_mut().take(stacks).enumerate() {
+                if line[stack * 4 + 1] == b' ' {
+                    *length += 1;
+                }
             }
         }
 
+        // produce string based on states resulting position
         let mut output = ArrayString::new();
-        for Stack(stack) in &*stacks {
-            output.push(stack.last().unwrap().0 as char);
+        for state in states.into_iter().take(stacks) {
+            let stack = state.0 as usize;
+            let offset = lengths[stack] - state.1;
+            let index = data_index_offsets[stack] + offset as usize - 1;
+            output.push(data.as_bytes()[index * stacks * 4 + stack * 4 + 1] as char);
         }
         output
     }
