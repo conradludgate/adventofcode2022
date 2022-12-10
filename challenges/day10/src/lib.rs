e3471c82ec284153cd54ad853e51959bd3adde95
Conrad Ludgate <conradludgate@gmail.com>
2022-12-10 06:42:00 +0000

    d10p1

---
+++ challenges/day10/src/lib.rs
@@ -0,0 +1,216 @@
+use aoc::{Challenge, Parser as ChallengeParser};
+use nom::{bytes::complete::tag, IResult, Parser};
+
+#[derive(Debug, PartialEq, Clone)]
+pub struct Solution(i32);
+
+impl ChallengeParser for Solution {
+    fn parse(input: &'static str) -> IResult<&'static str, Self> {
+        let mut x = 1;
+        let mut cycle = 1;
+        let mut total = 0;
+        for line in input.lines() {
+            let (cycles, newx) = match &line[..4] {
+                "addx" => {
+                    (2, line[5..].parse::<i32>().unwrap() + x)
+                },
+                _ => (1, x)
+            };
+
+            // ew
+            if (cycle + cycles + 19) / 40 > (cycle + 19) / 40 {
+                let c = ((cycle + cycles + 19) / 40) * 40 - 20;
+                total += x * c;
+            }
+
+            x = newx;
+            cycle += cycles;
+        }
+        Ok(("", Self(total)))
+    }
+}
+
+impl Challenge for Solution {
+    const NAME: &'static str = env!("CARGO_PKG_NAME");
+
+    type Output1 = i32;
+    fn part_one(self) -> Self::Output1 {
+        self.0
+    }
+
+    type Output2 = usize;
+    fn part_two(self) -> Self::Output2 {
+        0
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+    use aoc::{Challenge, Parser};
+
+    const INPUT: &str = "addx 15
+addx -11
+addx 6
+addx -3
+addx 5
+addx -1
+addx -8
+addx 13
+addx 4
+noop
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx -35
+addx 1
+addx 24
+addx -19
+addx 1
+addx 16
+addx -11
+noop
+noop
+addx 21
+addx -15
+noop
+noop
+addx -3
+addx 9
+addx 1
+addx -3
+addx 8
+addx 1
+addx 5
+noop
+noop
+noop
+noop
+noop
+addx -36
+noop
+addx 1
+addx 7
+noop
+noop
+noop
+addx 2
+addx 6
+noop
+noop
+noop
+noop
+noop
+addx 1
+noop
+noop
+addx 7
+addx 1
+noop
+addx -13
+addx 13
+addx 7
+noop
+addx 1
+addx -33
+noop
+noop
+noop
+addx 2
+noop
+noop
+noop
+addx 8
+noop
+addx -1
+addx 2
+addx 1
+noop
+addx 17
+addx -9
+addx 1
+addx 1
+addx -3
+addx 11
+noop
+noop
+addx 1
+noop
+addx 1
+noop
+noop
+addx -13
+addx -19
+addx 1
+addx 3
+addx 26
+addx -30
+addx 12
+addx -1
+addx 3
+addx 1
+noop
+noop
+noop
+addx -9
+addx 18
+addx 1
+addx 2
+noop
+noop
+addx 9
+noop
+noop
+noop
+addx -1
+addx 2
+addx -37
+addx 1
+addx 3
+noop
+addx 15
+addx -21
+addx 22
+addx -6
+addx 1
+noop
+addx 2
+addx 1
+noop
+addx -10
+noop
+noop
+addx 20
+addx 1
+addx 2
+addx 2
+addx -6
+addx -11
+noop
+noop
+noop";
+
+    #[test]
+    fn parse() {
+        let output = Solution::parse(INPUT).unwrap().1;
+        println!("{output:?}");
+    }
+
+    #[test]
+    fn part_one() {
+        let output = Solution::parse(INPUT).unwrap().1;
+        assert_eq!(output.part_one(), 13140);
+    }
+
+    #[test]
+    fn part_two() {
+        let output = Solution::parse(INPUT).unwrap().1;
+        assert_eq!(output.part_two(), 0);
+    }
+}
