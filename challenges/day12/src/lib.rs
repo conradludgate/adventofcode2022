0374a26a7506e0bf74bf6856c5540b2922df4833
Conrad Ludgate <conradludgate@gmail.com>
2022-12-12 08:36:45 +0000

    single bfs

--- challenges/day12/src/lib.rs
+++ challenges/day12/src/lib.rs
@@ -1,6 +1,6 @@
 use aoc::{Challenge, Parser as ChallengeParser};
 use nom::IResult;
-use pathfinding::directed::{astar, dijkstra};
+use pathfinding::directed::bfs;
 
 #[derive(Debug, PartialEq, Clone)]
 pub struct Solution {
@@ -29,74 +29,49 @@ impl ChallengeParser for Solution {
     }
 }
 
-impl Challenge for Solution {
-    const NAME: &'static str = env!("CARGO_PKG_NAME");
-
-    type Output1 = usize;
-    fn part_one(self) -> Self::Output1 {
+impl Solution {
+    fn solve(self, any: bool) -> usize {
         let Self { map, width, end, start } = self;
         let stride = width + 1;
-        astar::astar(
-            &start,
-            |&p| {
-                [p + 1, p + stride, p.wrapping_sub(1), p.wrapping_sub(stride)]
-                    .into_iter()
-                    // .inspect(|q| println!("{q}"))
-                    .filter(move |&q| {
-                        if q >= map.len() {
-                            return false;
-                        }
-                        let mut vp = map[p];
-                        let mut vq = map[q];
-                        if vp == b'S' {
-                            vp = b'a'
-                        }
-                        if vq == b'E' {
-                            vq = b'z'
-                        }
-                        vq != b'\n' && vq <= vp + 1
-                    })
-                    .map(|q| (q, 1))
-            },
-            |p| {
-                let dist = end.abs_diff(*p);
-                dist / stride + dist % stride
-            },
-            |p| *p == end,
-        )
-        .unwrap()
-        .1
-    }
-
-    type Output2 = usize;
-    fn part_two(self) -> Self::Output2 {
-        let Self { map, width, end, .. } = self;
-        let stride = width + 1;
-        dijkstra::dijkstra(
+        // pathfind from E to S (or any 'a' if the flag is set)
+        bfs::bfs(
             &end,
             |&p| {
+                let mut vp = map[p];
+                if vp == b'E' {
+                    vp = b'z'
+                }
+                // try step in any right, down, left, up direction
                 [p + 1, p + stride, p.wrapping_sub(1), p.wrapping_sub(stride)]
                     .into_iter()
+                    .filter(|&q| q < map.len())
                     .filter(move |&q| {
-                        if q >= map.len() {
-                            return false;
-                        }
-                        let mut vp = map[p];
                         let mut vq = map[q];
-                        if vp == b'E' {
-                            vp = b'z'
-                        }
                         if vq == b'S' {
                             vq = b'a'
                         }
                         vp != b'\n' && vp <= vq + 1
                     })
-                    .map(|q| (q, 1))
             },
-            |p| map[*p] == b'S' || map[*p] == b'a',
+            |p| *p == start || (any && map[*p] == b'a'),
         )
         .unwrap()
-        .1
+        .len()
+            - 1
+    }
+}
+
+impl Challenge for Solution {
+    const NAME: &'static str = env!("CARGO_PKG_NAME");
+
+    type Output1 = usize;
+    fn part_one(self) -> Self::Output1 {
+        self.solve(false)
+    }
+
+    type Output2 = usize;
+    fn part_two(self) -> Self::Output2 {
+        self.solve(true)
     }
 }
 
