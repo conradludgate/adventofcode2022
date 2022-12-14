6b87fa1a7d49b762822b85840cc4ad34a30e7e55
Conrad Ludgate <conradludgate@gmail.com>
2022-12-14 09:09:21 +0000

    part2

--- challenges/day14/src/lib.rs
+++ challenges/day14/src/lib.rs
@@ -68,7 +68,7 @@ impl Challenge for Solution {
         }
         let width = xend - xstart;
 
-        dbg!(xstart..xend, yend);
+        // dbg!(xstart..xend, yend);
 
         let mut grid = vec![GridSpot::Air; (width * yend) as usize];
 
@@ -129,28 +129,93 @@ impl Challenge for Solution {
             }
 
             i += 1;
-
-            // for line in grid.chunks(width as usize) {
-            //     for v in line {
-            //         print!("{v}");
-            //     }
-            //     println!()
-            // }
         }
-
-        for line in grid.chunks(width as usize) {
-            for v in line {
-                print!("{v}");
-            }
-            println!()
-        }
-
         i
     }
 
     type Output2 = usize;
     fn part_two(self) -> Self::Output2 {
-        0
+        // let mut xstart = 500;
+        // let mut xend = 501;
+        let mut yend = 1;
+
+        for &i in &self.0 {
+            if let LineSegment::Point(Point(_, y)) = i {
+                // xstart = u16::min(x, xstart);
+                // xend = u16::max(x + 1, xend);
+                // yrange.start = u16::min(y, yrange.start);
+                yend = u16::max(y + 1, yend);
+            }
+        }
+        let xstart = 500 - yend - 1;
+        let xend = 500 + yend + 2;
+        yend += 2;
+
+        let width = xend - xstart;
+        // dbg!(xstart..xend, yend);
+
+        let mut grid = vec![GridSpot::Air; (width * yend) as usize];
+
+        for &[a, b] in self.0.array_windows() {
+            let (a, b) = match (a, b) {
+                (LineSegment::Point(a), LineSegment::Point(b)) => (a, b),
+                _ => continue,
+            };
+
+            if a.0 == b.0 {
+                let x = a.0 - xstart;
+                let y = if a.1 > b.1 { b.1..=a.1 } else { a.1..=b.1 };
+                for y in y {
+                    grid[(y * width + x) as usize] = GridSpot::Rock;
+                }
+            } else if a.1 == b.1 {
+                let y = a.1;
+                let x = if a.0 > b.0 {
+                    (b.0 - xstart)..=(a.0 - xstart)
+                } else {
+                    (a.0 - xstart)..=(b.0 - xstart)
+                };
+                for x in x {
+                    grid[(y * width + x) as usize] = GridSpot::Rock;
+                }
+            }
+        }
+        for x in 0..width {
+            grid[((yend - 1) * width + x) as usize] = GridSpot::Rock;
+        }
+
+        let mut i = 0;
+        'outer: loop {
+            let mut x = 500 - xstart;
+            let mut y = 0;
+            loop {
+                if y + 1 == yend {
+                    // fallen off grid
+                    break 'outer;
+                }
+                let k = ((y + 1) * width + x) as usize;
+                if grid[k] == GridSpot::Air {
+                    y += 1;
+                } else if grid[k - 1] == GridSpot::Air {
+                    y += 1;
+                    x -= 1;
+                } else if grid[k + 1] == GridSpot::Air {
+                    y += 1;
+                    x += 1;
+                } else {
+                    if x == 500 - xstart && y == 0 {
+                        break 'outer;
+                    }
+                    // settled
+                    grid[(y * width + x) as usize] = GridSpot::Sand;
+                    break
+                }
+            }
+
+            i += 1;
+        }
+
+        i + 1
     }
 }
 
@@ -194,6 +259,6 @@ mod tests {
     #[test]
     fn part_two() {
         let output = Solution::parse(INPUT).unwrap().1;
-        assert_eq!(output.part_two(), 0);
+        assert_eq!(output.part_two(), 93);
     }
 }
