66b79899f3ede50991457fedf60e4f3cc9333e55
Conrad Ludgate <conradludgate@gmail.com>
2022-12-18 08:49:22 +0000

    17p1

--- challenges/day17/src/lib.rs
+++ challenges/day17/src/lib.rs
@@ -15,104 +15,75 @@ impl ChallengeParser for Solution {
 // bottom of the pattern is the left-most septet
 // lines are length 7, but we pad them to 8
 #[allow(clippy::unusual_byte_groupings)]
-const PIECES: [(u32, u32, u32); 5] = [
-    (0b00011110_00000000_00000000_00000000, 4, 1), // horizontal line
-    (0b00001000_00011100_00001000_00000000, 3, 3), // cross
-    (0b00011100_00000100_00000100_00000000, 3, 3), // L
-    (0b00010000_00010000_00010000_00010000, 1, 4), // vertical line
-    (0b00011000_00011000_00000000_00000000, 2, 2), // square
+const PIECES: [([u8; 4], u8, u8); 5] = [
+    ([0b00011110, 0b00000000, 0b00000000, 0b00000000], 4, 1), // horizontal line
+    ([0b00001000, 0b00011100, 0b00001000, 0b00000000], 3, 3), // cross
+    ([0b00011100, 0b00000100, 0b00000100, 0b00000000], 3, 3), // L
+    ([0b00010000, 0b00010000, 0b00010000, 0b00010000], 1, 4), // vertical line
+    ([0b00011000, 0b00011000, 0b00000000, 0b00000000], 2, 2), // square
 ];
 
 impl Challenge for Solution {
     const NAME: &'static str = env!("CARGO_PKG_NAME");
 
-    type Output1 = u32;
+    type Output1 = usize;
     fn part_one(self) -> Self::Output1 {
-        let mut bitset = Vec::<u32>::with_capacity(2022 * 7 / 2 / 32); // that is not a date
-        bitset.push(0);
+        let mut bitset = Vec::<u8>::with_capacity(2022 * 7 / 2 / 32); // that is not a date
+        bitset.resize(3 + 4, 0);
 
         // index in our jet cycle
         let mut i = 0;
 
         // current height of our stack
-        let mut stack_height = 0u32;
+        let mut stack_height = 0usize;
 
         for piece in 0..2022 {
             // current offset from the left of our falling piece
             let mut x = 2;
             let mut y = stack_height + 3;
-            dbg!(y);
-            let (bits, width, height) = PIECES[piece % 5];
-            let (mut a, mut b) = match y % 4 {
-                0 => (0, bits),
-                1 => (bits >> 8, bits << 24),
-                2 => (bits >> 16, bits << 16),
-                3.. => (bits >> 24, bits << 8),
-            };
-            // let mut j = bitset.len() - 1;
+            let (mut bits, width, height) = PIECES[piece % 5];
 
             loop {
-                // for p in b.to_le_bytes() {
-                //     println!("{p:07b}");
-                // }
-                // for p in a.to_le_bytes() {
-                //     println!("{p:07b}");
-                // }
-                // println!();
-
-                let mut j = (y / 4) as usize;
                 // move horizontally
                 {
                     let jet_left = self.0[i] == b'<';
                     i += 1;
                     i %= self.0.len();
 
-                    let (x1, a1, b1) = if jet_left && x > 0 {
-                        (x - 1, a << 1, b << 1)
+                    let (x1, new_bits) = if jet_left && x > 0 {
+                        (x - 1, bits.map(|x| x << 1))
                     } else if !jet_left && x + width < 7 {
-                        (x + 1, a >> 1, b >> 1)
+                        (x + 1, bits.map(|x| x >> 1))
                     } else {
-                        (x, a, b)
+                        (x, bits)
                     };
-                    if j >= bitset.len() || a1 & bitset[j] == 0 {
-                        (a, b, x) = (a1, b1, x1)
+                    let chunk = u32::from_ne_bytes(bitset[y..y+4].try_into().unwrap());
+                    let mask = u32::from_ne_bytes(new_bits);
+
+                    if chunk & mask == 0 {
+                        (x, bits) = (x1, new_bits);
                     }
                 }
-
-                // for p in b.to_le_bytes() {
-                //     println!("{p:07b}");
-                // }
-                // for p in a.to_le_bytes() {
-                //     println!("{p:07b}");
-                // }
-                // println!();
 
                 // attempt to move down vertically
                 {
                     if y > 0 {
-                        let (a1, b1) = if (a >> 24) > 0 {
-                            (a >> 24, a << 8)
-                        } else {
-                            (a << 8 | b >> 24, b << 8)
-                        };
-                        if j >= bitset.len() || a1 & bitset[j] == 0 {
+                        let chunk = u32::from_ne_bytes(bitset[y-1..y+3].try_into().unwrap());
+                        let mask = u32::from_ne_bytes(bits);
+                        if chunk & mask == 0 {
                             y -= 1;
-                            (a, b) = (a1, b1)
-                        } else {
-                            stack_height = y + height;
-                            bitset[j] |= a;
-                            if j + 1 == bitset.len() {
-                                bitset.push(b);
-                            } else {
-                                bitset[j + 1] |= b;
-                            }
-                            break;
+                            continue;
                         }
-                    } else {
-                        stack_height = height;
-                        bitset[0] |= a;
-                        break;
                     }
+
+                    for (c, m) in bitset[y..y+4].iter_mut().zip(bits) {
+                        *c |= m;
+                    }
+                    stack_height = usize::max(stack_height, y + height as usize);
+                    if bitset.len() < stack_height + 7 {
+                        bitset.resize(stack_height + 7, 0)
+                    }
+                    break
                 }
             }
         }
