96ce78198e69ef0415af1265923a92e13ebd232d
Conrad Ludgate <conradludgate@gmail.com>
2022-12-19 08:23:44 +0000

    19p1 maybe

--- challenges/day19/src/lib.rs
+++ challenges/day19/src/lib.rs
@@ -1,5 +1,6 @@
 use aoc::{Challenge, Parser as ChallengeParser};
 use nom::{bytes::complete::tag, IResult, Parser};
+use pathfinding::prelude::dijkstra;
 
 #[derive(Debug, PartialEq, Clone, Copy)]
 pub struct Blueprint {
@@ -32,12 +33,103 @@ impl ChallengeParser for Solution {
             bp.push(Blueprint {
                 ore: ore_cost.parse().unwrap(),
                 clay: clay_cost.parse().unwrap(),
-                obsidian: (obsidian_cost_ore.parse().unwrap(), obsidian_cost_clay.parse().unwrap()),
-                geode: (geode_cost_ore.parse().unwrap(), geode_cost_obsidian.parse().unwrap()),
+                obsidian: (
+                    obsidian_cost_ore.parse().unwrap(),
+                    obsidian_cost_clay.parse().unwrap(),
+                ),
+                geode: (
+                    geode_cost_ore.parse().unwrap(),
+                    geode_cost_obsidian.parse().unwrap(),
+                ),
             });
         }
 
         Ok(("", Self(bp)))
+    }
+}
+
+impl Blueprint {
+    fn solve(self) -> usize {
+        #[derive(Debug, Clone, Copy, PartialEq, Hash, Eq)]
+        struct State {
+            time: usize,
+            ore: usize,
+            clay: usize,
+            obsidian: usize,
+            geodes: usize,
+            ore_robots: usize,
+            clay_robots: usize,
+            obsidian_robots: usize,
+            geode_robots: usize,
+        }
+
+        let res = dijkstra(
+            &State {
+                time: 0,
+                ore: 0,
+                clay: 0,
+                obsidian: 0,
+                geodes: 0,
+                ore_robots: 1,
+                clay_robots: 0,
+                obsidian_robots: 0,
+                geode_robots: 0,
+            },
+            |s| {
+                let base = State {
+                    time: s.time + 1,
+                    ore: s.ore + s.ore_robots,
+                    clay: s.clay + s.clay_robots,
+                    obsidian: s.obsidian + s.obsidian_robots,
+                    geodes: s.geodes + s.geode_robots,
+                    ..*s
+                };
+
+                let cost = 24 - s.geode_robots;
+
+                // needs to be lazy to avoid underflows
+                #[allow(clippy::unnecessary_lazy_evaluations)]
+                [
+                    (s.ore >= self.ore).then(|| State {
+                        ore_robots: s.ore_robots + 1,
+                        ore: base.ore - self.ore,
+                        ..base
+                    }),
+                    (s.ore >= self.clay).then(|| State {
+                        clay_robots: s.clay_robots + 1,
+                        ore: base.ore - self.clay,
+                        ..base
+                    }),
+                    (s.ore >= self.obsidian.0 && s.clay >= self.obsidian.1).then(|| State {
+                        obsidian_robots: s.obsidian_robots + 1,
+                        ore: base.ore - self.obsidian.0,
+                        clay: base.clay - self.obsidian.1,
+                        ..base
+                    }),
+                    (s.ore >= self.geode.0 && s.obsidian >= self.geode.1).then(|| State {
+                        geode_robots: s.geode_robots + 1,
+                        ore: base.ore - self.geode.0,
+                        obsidian: base.obsidian - self.geode.1,
+                        ..base
+                    }),
+                ]
+                .into_iter()
+                .flatten()
+                .chain(std::iter::once(base))
+                .map(move |s| (s, cost))
+            },
+            |s| s.time == 23,
+        )
+        .unwrap();
+
+        let mut count = 0;
+        for i in res.0 {
+            count += i.geode_robots;
+        }
+        dbg!(count);
+
+        // 24 * 24 - dbg!(res.1)
+        count
     }
 }
 
@@ -46,7 +138,11 @@ impl Challenge for Solution {
 
     type Output1 = usize;
     fn part_one(self) -> Self::Output1 {
-        0
+        let mut sum = 0;
+        for (i, bp) in self.0.into_iter().enumerate() {
+            sum += (i + 1) * bp.solve()
+        }
+        sum
     }
 
     type Output2 = usize;
@@ -73,7 +169,7 @@ Blueprint 2: Each ore robot costs 2 ore. Each clay robot costs 3 ore. Each obsid
     #[test]
     fn part_one() {
         let output = Solution::parse(INPUT).unwrap().1;
-        assert_eq!(output.part_one(), 0);
+        assert_eq!(output.part_one(), 33);
     }
 
     #[test]
--- challenges/day19/src/main.rs
+++ challenges/day19/src/main.rs
@@ -2,6 +2,6 @@ use day19::Solution;
 
 fn main() {
     let input = include_str!("../input.txt");
-    // aoc::check::<Solution>(input);
-    aoc::run::<Solution>(input);
+    aoc::check::<Solution>(input);
+    // aoc::run::<Solution>(input);
 }
